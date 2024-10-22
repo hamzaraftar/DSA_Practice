4615d13a0137e4fe9b74e24cb62f62ac93903357
Hamza Asghar <hamzaraftar765@gmail.com>
2024-10-22 08:56:34 -0700

    revisoin array

--- Array/creation/index.cpp
+++ Array/creation/index.cpp
@@ -3,17 +3,18 @@ using namespace std;
 int main()
 {
     int arr[20], size;
-    cout << "Enter size of array :" << endl;
+    cout << "Enter array size :" << endl;
     cin >> size;
-    cout << "Enter elements of array :" << endl;
+    cout << "Enter elements of array : " << endl;
     for (int i = 0; i < size; i++)
     {
         cin >> arr[i];
     }
-    cout << "The elements in array are :" << endl;
+    cout << "The element of array are :" << endl;
     for (int i = 0; i < size; i++)
     {
         cout << arr[i] << endl;
     }
+
     return 0;
 }
