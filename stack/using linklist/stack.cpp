72334c91ea08bd3232948ab5c69bda09fd607cb8
Hamza Asghar <hamzaraftar765@gmail.com>
2025-04-19 19:20:47 +0500

    some change

---
+++ stack/using linklist/stack.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Node.cpp"
+using namespace std;
+
+class Stack
+{
+private:
+public:
+    void push() {}
+    void pop() {}
+    void peek() {}
+    void Display() {}
+};
+
+int main() {}
