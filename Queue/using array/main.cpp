b4d351da81752d453a63e5546e9a4d64783dcc61
Hamza Asghar <hamzaraftar765@gmail.com>
2025-04-23 21:57:34 +0500

    dequequ method of Queqe

--- Queue/using array/main.cpp
+++ Queue/using array/main.cpp
@@ -21,4 +21,21 @@ void enqueue(int x)
         queue[rear] = x;
     }
 }
+
+void dequeue()
+{
+    if (front == -1 && rear == -1)
+    {
+        cout << "Queue is empty" << endl;
+    }
+    else if (front == rear)
+    {
+        front = rear = -1;
+    }
+    else
+    {
+        cout << "Dequeue element is : " << queue[front] << endl;
+        front++;
+    }
+}
 int main() {}
