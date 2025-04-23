6b815ee0a952aa3ac1e6a8af339de2fb4880cd59
Hamza Asghar <hamzaraftar765@gmail.com>
2025-04-23 21:37:10 +0500

    enquequ method of Queqe

---
+++ Queue/using array/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int queue[5];
+int front = -1;
+int rear = -1;
+
+void enqueue(int x)
+{
+    if (rear == 4)
+    {
+        cout << "Overflow" << endl;
+    }
+    else if (front == -1 && rear == -1)
+    {
+        front = rear = 0;
+        queue[rear] = x;
+    }
+    else
+    {
+        rear++;
+        queue[rear] = x;
+    }
+}
+int main() {}
