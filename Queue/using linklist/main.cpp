825670429379dd2d7ba6924a84e4f54368bc367f
Hamza Asghar <hamzaraftar765@gmail.com>
2025-04-24 21:37:39 +0500

    enqueue method for queque using linklist

--- Queue/using linklist/main.cpp
+++ Queue/using linklist/main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
 using namespace std;
+#include "Node.cpp"
+
+class Queue
+{
+private:
+    Node *front = 0;
+    Node *rear = 0;
+
+public:
+    void enqueue(int x)
+    {
+        Node *newNode = new Node();
+        newNode->set(x);
+        newNode->setNext(0);
+        if (front == 0 && rear == 0)
+        {
+            front = rear = newNode;
+        }
+        else
+        {
+            rear->setNext(newNode);
+            rear = newNode;
+        }
+    }
+};
 
 int main()
 {
