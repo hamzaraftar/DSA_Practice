e00c2c708f268089c9645c879494ed71ee10f9d4
Hamza Asghar <hamzaraftar765@gmail.com>
2025-05-01 09:09:59 +0500

    complete

--- Queue/Assignment/cs301.cpp
+++ Queue/Assignment/cs301.cpp
@@ -60,8 +60,114 @@ public:
             rear = newProduct;
         }
     }
+
+    int dequeue()
+    {
+        if (front == nullptr)
+        {
+            cout << "Queue is empty " << endl;
+            return -1;
+        }
+        Product *temp = front;
+        int serial = temp->getData();
+        int pid = temp->getProductId();
+
+        cout << "Solid product with serial :" << serial << "and ID :" << pid << endl;
+
+        front = front->getNext();
+
+        if (front == nullptr)
+        {
+            rear = nullptr;
+        }
+        delete temp;
+        return serial;
+    }
+
+    void displayFront()
+    {
+        if (front == nullptr)
+        {
+            cout << "Queue is empyt" << endl;
+        }
+        else
+        {
+            cout << "First product in queue -> Serial: " << front->getData() << "Product id: " << front->getProductId() << endl;
+        }
+    }
+
+    void displayRear()
+    {
+        if (rear == nullptr)
+        {
+            cout << "Queue is empyt" << endl;
+        }
+        else
+        {
+            cout << "Last product in queue -> Serial: " << rear->getData() << "Product id: " << rear->getProductId() << endl;
+        }
+    }
 };
 int main()
 {
+    string studentID = "BC230403914";
+    cout << "Student ID : " << studentID << endl;
+    cout << "==========================================" << endl;
+
+    string lastThreeDigits = studentID.substr(studentID.length() - 4);
+    int smartphonecout = lastThreeDigits[0] - '0';
+    int tabletcout = lastThreeDigits[1] - '0';
+    int watchcount = lastThreeDigits[2] - '0';
+
+    string lastFourDigits = studentID.substr(studentID.length() - 4);
+    int productid = stoi(lastFourDigits);
+
+    Queue inventory;
+    int serialNumber = 1;
+
+    cout << smartphonecout << "smartphones are added to inventory : " << endl;
+    for (int i = 0; i < smartphonecout; i++)
+    {
+        cout << "Added -> Serial : " << serialNumber << "Product ID: " << productid << endl;
+        inventory.enqueue(serialNumber, productid);
+        serialNumber++;
+        productid++;
+    }
+    cout << endl;
+
+    cout << tabletcout << 'tablets are added to inventory : ' << endl;
+    for (int i = 0; i < tabletcout; i++)
+    {
+        cout << "Added -> Serial : " << serialNumber << "Product ID" << productid << endl;
+        inventory.enqueue(serialNumber, productid);
+        serialNumber++;
+        productid++;
+    }
+    cout << endl;
+
+    cout << watchcount << 'watches are added to inventory : ' << endl;
+    for (int i = 0; i < watchcount; i++)
+    {
+        cout << "Added -> Serial : " << serialNumber << "Product ID" << productid << endl;
+        inventory.enqueue(serialNumber, productid);
+        serialNumber++;
+        productid++;
+    }
+    cout << endl;
+
+    cout << "=========================" << endl;
+    inventory.displayFront();
+    cout << "=========================" << endl;
+    inventory.displayFront();
+    cout << "=========================" << endl;
+
+    cout << 'Selling products are' << endl;
+    inventory.dequeue();
+    inventory.dequeue();
+    cout << "=========================" << endl;
+
+    inventory.displayFront();
+    cout << "=========================" << endl;
+
     return 0;
 }
