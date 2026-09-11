1beb29adf5e10b90ab1e342ac0eed51c51e5c0f3
Hamza Asghar <hamzaraftar765@gmail.com>
2026-09-11 15:46:57 +0500

    solving minimum sum subarray of size k

--- dsa/sliding_window.py
+++ dsa/sliding_window.py
@@ -10,6 +10,7 @@ print(best)
 '''
 
 #--------------------------------------- 2. Average of Subarrays of Size K
+'''
 array = [1, 3, 2, 6, -1, 4]
 k = 3
 averages = []
@@ -22,3 +23,17 @@ for r in range(k, len(array)):
     average = cur / k
     averages.append(average)
 print(averages)    
+'''
+
+# ------------------------------------ 3. Minimum Sum Subarray of Size K
+'''
+array = [2, 3, 4, 1, 5]
+k = 2
+
+current = best = sum(array[:2])
+for r in range(k , len(array)):
+    current = current + array[r] - array[r - k]
+    best = min(current,best)
+
+print(best)    
+'''
