521313bc13d3ae78fc43d6a4504fdea6d3ed3736
Hamza Asghar <hamzaraftar765@gmail.com>
2026-09-15 16:39:50 +0500

    fex

--- dsa/prefix_sum.py
+++ dsa/prefix_sum.py
@@ -1,11 +1,13 @@
 # --------------------------------  1. Build a Prefix Sum Array
 '''
 array = [2, 4, 1, 3, 5]
-prefix = []
+# making list of same size as given in question so i assess there index
+prefix = [0] * len(array)
+prefix[0] = array[0]
 
-prefix = [array[0]] 
-for i in range(1,len(array)):
+for i in range(1, len(array)):
     # previouse ma new wala add kar na ha 
-    prefix.append( prefix[i-1] + array[i])
+    prefix[i] = prefix[i-1] + array[i]
+
 print(prefix)
 '''
