c4dd10e776d8a7751a604e9f76f9534e05401b90
Hamza Asghar <hamzaraftar765@gmail.com>
2026-09-16 16:44:38 +0500

    range sum query

--- dsa/prefix_sum.py
+++ dsa/prefix_sum.py
@@ -11,3 +11,17 @@ for i in range(1, len(array)):
 
 print(prefix)
 '''
+
+# ------------------------------------- 2. Range Sum Query
+# Create the prefix sum and then find the sum from index 1 to index 3.
+'''
+array = [2, 4, 1, 3, 5]
+prefix = [0] * len(array)
+
+prefix[0] = array[0]
+for i in range(1 , len(array)):
+    prefix[i] = prefix[i - 1] + array[i]
+
+answer = prefix[3] - prefix[0]
+print(answer)  
+'''
