717bab29ae8065ec90ba113fd445281bf206f9ab
Hamza Asghar <hamzaraftar765@gmail.com>
2026-09-17 16:49:35 +0500

    search insert position

--- dsa/binary_search.py
+++ dsa/binary_search.py
@@ -15,4 +15,25 @@ while left <= right:
         left = mid + 1
     else:
         right = mid - 1       
-'''        
+'''
+
+# ----------------------------  2 Search Insert Position
+'''
+array = [1, 3, 5, 6]
+target = 2
+
+left = 0
+right = len(array) - 1
+answer = len(array)   
+
+while left <= right:
+    mid = (left + right) // 2
+
+    if array[mid] >= target:
+        answer = mid
+        right = mid - 1
+    else:
+        left = mid + 1
+
+print(answer)
+'''
