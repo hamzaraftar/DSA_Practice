040ba156e00f5eadc828802a3f69af4ee2160ebb
Hamza Asghar <hamzaraftar765@gmail.com>
2026-09-19 15:31:54 +0500

    find the floor of a number

--- dsa/binary_search.py
+++ dsa/binary_search.py
@@ -37,3 +37,28 @@ while left <= right:
 
 print(answer)
 '''
+
+# ------------------------------ 3 Find First and Last Position
+# array = [1, 2, 2, 2, 3, 4]
+# target = 2
+
+
+
+# --------------------------------------- 4 Find the Floor of a Number
+'''
+array = [1, 3, 5, 7, 9]
+target = 6
+
+left = 0
+right = len(array) - 1
+answer = 0
+
+while left <= right:
+    mid = (left + right ) // 2
+    if array[mid] <= target:
+        answer = array[mid]
+        left = mid + 1
+    else:
+        right = mid - 1    
+print(answer)        
+'''
